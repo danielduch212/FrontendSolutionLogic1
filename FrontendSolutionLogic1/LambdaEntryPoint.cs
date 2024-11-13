beef7a0a7262bdd000cd3598d5227e02183c161f
Daniel Duchnowicz <72360092+danielduch212@users.noreply.github.com>
2024-11-13 21:02:54 +0100

    Changed

---
+++ FrontendSolutionLogic1/LambdaEntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace FrontendSolutionLogic1
+{
+    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IHostBuilder builder)
+        {
+            base.Init(builder);
+        }
+    }
+}
--- FrontendSolutionLogic1/WeatherForecast.cs
+++ FrontendSolutionLogic1/WeatherForecast.cs
@@ -2,12 +2,9 @@ namespace FrontendSolutionLogic1
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
+            public DateOnly Date { get; set; }
+            public int TemperatureC { get; set; }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+            public string? Summary { get; set; }
     }
 }
